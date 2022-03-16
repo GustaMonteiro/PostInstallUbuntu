2d37d9bb15adec7fda8e438182d8466f409abb0e
Gustavo Monteiro <gustavo.monteiro@hp.com>
2022-03-16 07:03:39 -0700

    Changed after testing

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-touch ~/.vimrc
+mkdir -p /home/$USER/.vim/pack/git-plugins/start/
+mkdir -p /home/$USER/.vim/pack/coc/start
+mkdir /home/$USER/.fonts
+cp .vimrc /home/$USER
+cp coc.nvimrc /home/$USER/.vim
 
-mkdir -p ~/.vim/pack/git-plugins/start/
-mkdir -p ~/.vim/pack/coc/start
-mkdir ~/.fonts
-cp .vimrc ~
-cp coc.nvimrc ~/.vim
+sudo apt install curl -y
 
-cd ~/.vim/pack/git-plugins/start/
+cd /home/$USER/.vim/pack/git-plugins/start/
 git clone https://github.com/mhinz/vim-startify
 git clone https://github.com/rafi/awesome-vim-colorschemes
 git clone https://github.com/Yggdroot/indentLine
@@ -20,23 +20,23 @@ git clone https://github.com/preservim/nerdcommenter
 git clone https://github.com/dense-analysis/ale
 git clone https://github.com/sheerun/vim-polyglot
 
-cd ~/.fonts
+cd /home/$USER/.fonts
 
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf
 
 fc-cache
 
-cd ~
+cd /home/$USER
 
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-source ~/.bashrc
+source /home/$USER/.bashrc
 
 nvm install v13.6.0
 
-cd ~/.vim/pack/coc/start
+cd /home/$USER/.vim/pack/coc/start
 git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1
 
-cd ~/.vim
+cd /home/$USER/.vim
