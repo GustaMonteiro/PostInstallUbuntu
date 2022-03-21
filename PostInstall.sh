aa30a4d7ed44ab70e7b9c96a63766211b8f5ae9f
Gustavo Monteiro <gcvm6070@gmail.com>
2022-03-21 15:30:44 -0300

    Added gnome-tweaks + Qogir theme

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo apt install gcc git vim curl dconf-editor -y
+sudo apt install gcc git vim curl dconf-editor gnome-tweaks -y
 sudo snap install spotify
 
 cd /home/$USER/Downloads
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+git clone https://github.com/vinceliuice/Qogir-theme.git
+cd Qogir-theme
+./install.sh
