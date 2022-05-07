2235bf624c5cb3998f0985a7763cd743687f3094
Gustavo Monteiro <gcvm6070@gmail.com>
2022-05-06 21:12:30 -0300

    fixed ConfigureZsh.sh

--- ConfigureZsh.sh
+++ ConfigureZsh.sh
@@ -4,7 +4,7 @@ sudo apt install zsh curl -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sh -c "$(curl -fsSL https://git.io/zinit-install)"
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 echo "zinit light zdharma/fast-syntax-highlighting" >> /home/$USER/.zshrc 
 
