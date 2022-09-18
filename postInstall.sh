7d210e3f77d3e088a14ee25f4b628f5d430ca0a1
GustaMonteiro <gcvm6070@gmail.com>
2022-09-17 21:41:21 -0300

    Update zsh: use powerlevel10k instead of oh-my-zsh

--- postInstall.sh
+++ postInstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/postinstall"
 
--- zsh.sh
+++ zsh.sh
@@ -1,15 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo apt install zsh curl -y
+ZSH_PLUGINS_PATH="$HOME/.zsh-plugins"
+ZSHRC="$HOME/.zshrc"
+DIRETORIO_FONTS="$HOME/.fonts"
+P10K_PATH="$HOME/.p10k-dir"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir -p $ZSH_PLUGINS_PATH
+mkdir -p $DIRETORIO_FONTS
+mkdir -p $P10K_PATH
 
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+LISTA_FONTES=(
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+)
 
-echo "zinit light zdharma/fast-syntax-highlighting" >> $HOME/.zshrc 
+instalar_zsh () {
+  sudo apt update && sudo apt install zsh curl wget git -y
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $P10K_PATH
+  git clone https://github.com/z-shell/F-Sy-H $ZSH_PLUGINS_PATH/fast-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS_PATH/zsh-autosuggestions
+}
 
-echo "zinit light zsh-users/zsh-autosuggestions" >> $HOME/.zshrc 
+instalar_fontes () {
+  for font in ${LISTA_FONTES[@]}; do
+    wget -c $font -P $DIRETORIO_FONTS
+  done
+  fc-cache
+}
 
-echo "zinit light zsh-users/zsh-completions" >> $HOME/.zshrc 
+exports_e_sources () {
+  echo "source $ZSH_PLUGINS_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $ZSHRC
+  echo "source $ZSH_PLUGINS_PATH/fast-syntax-highlighting/F-Sy-H.plugin.zsh" >> $ZSHRC
+  echo "source $P10K_PATH/powerlevel10k.zsh-theme" >> $ZSHRC
+}
+
+instalar_zsh
+instalar_fontes
+exports_e_sources
 
 chsh -s $(which zsh)
